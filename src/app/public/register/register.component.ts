3e7c0d68ff48e2e2b6a74210a621fa2d1fee68a0
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-29 19:39:46 +0200

    fix register account

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +11,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      first_name: ['', Validators.required],
-      last_name: ['', Validators.required],
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       password_confirmation: ['', Validators.required],
@@ -26,7 +30,10 @@ export class RegisterComponent implements OnInit {
     const formData = this.form.getRawValue();
 
     this.http.post('http://localhost:8000/api/register', formData).subscribe(
-      (result) => console.log(result),
+      (result) => {
+        console.log(result);
+        this.router.navigate(['/']);
+      },
       (err) => console.log(err)
     );
   }
