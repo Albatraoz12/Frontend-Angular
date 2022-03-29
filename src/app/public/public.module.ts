351419d939d5e16e811168718b7d42d1b95cb4a8
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-29 16:51:41 +0200

    add public component

---
+++ src/app/public/public.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PublicComponent } from './public.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    PublicComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+})
+export class PublicModule {}
