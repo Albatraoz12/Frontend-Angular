75804e873218aef29c1efe828ff3e8a33509342f
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-06 14:44:28 +0200

    Updated Envoriments.ts
    Removed unussed code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,6 @@
 
 export const environment = {
   production: false,
-  // spoonUrl: 'https://api.spoonacular.com/recipes/complexSearch?apiKey=',
-  // spoonUrlSinge: 'https://api.spoonacular.com/recipes/',
-  // spoonRandom: 'https://api.spoonacular.com/recipes/random?',
-  // apiKey: '585a45e21e2547dfb6c6cd369e6a76e7',
-  // apiKey: '050742ec9ef64a719d760c22b2903868', //apiKey 2
-  // heroUrl: 'https://dinorage-api.herokuapp.com/api/',
 };
 
 /*
